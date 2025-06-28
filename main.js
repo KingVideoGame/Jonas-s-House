7ead3852c2694a6fb7bd805bff0e3b6574de69ca
KingVideoGame <119747783+KingVideoGame@users.noreply.github.com>
2025-06-28 14:06:23 -0500

    some of the code written for getting the sprite to stop when an arrow key is let go, still doesn't work yet

--- main.js
+++ main.js
@@ -40,23 +40,55 @@ document.getElementById("entryScreen").addEventListener("keypress", function(eve
 
 //Keypress for ethanSprite
 document.getElementById("livingRoom").addEventListener("keydown", function(event) {
-  const sprites = document.querySelectorAll(".ethanSpriteVideo");
+  last_button_pressed = (event.code === "ArrowUp" ||
+                   event.code === "ArrowDown" ||
+                   event.code === "ArrowLeft" ||
+                   event.code === "ArrowRight");
+  const videoSprites = document.querySelectorAll(".ethanSpriteVideo");
   if (event.code === "ArrowUp") {
     console.log("Up arrow pressed!");
-    sprites.forEach(sprite => sprite.hidden = true);
+    videoSprites.forEach(sprite => sprite.hidden = true);
     document.getElementById("ethanRunningBackward").hidden = false;
+    last_button_pressed = "ArrowUp";
+    button_pressed = true;
   } if (event.code === "ArrowDown") {
     console.log("Down arrow pressed!");
-    sprites.forEach(sprite => sprite.hidden = true);
+    videoSprites.forEach(sprite => sprite.hidden = true);
     document.getElementById("ethanRunningForward").hidden = false;
+    button_pressed = true;
+    last_button_pressed = "ArrowDown";
+    button_pressed = true;
   } if (event.code === "ArrowLeft") {
     console.log("Left arrow pressed!");
-    sprites.forEach(sprite => sprite.hidden = true);
+    videoSprites.forEach(sprite => sprite.hidden = true);
     document.getElementById("ethanRunningLeft").hidden = false;
+    last_button_pressed = "ArrowLeft";
+    button_pressed = true;
   } if (event.code === "ArrowRight") {
     console.log("Right arrow pressed!");
-    sprites.forEach(sprite => sprite.hidden = true);
+    videoSprites.forEach(sprite => sprite.hidden = true);
     document.getElementById("ethanRunningRight").hidden = false;
-    const button = document.getElementById("ArrowUp");
+    last_button_pressed = "ArrowRight";
+    button_pressed = true;
   }
 });
+document.getElementById("livingRoom").addEventListener("keyup", function(event) {
+    const imageSprites = document.querySelectorAll(".ethanSpriteImage"); 
+    switch (event.code) {
+      case "ArrowUp": 
+    imageSprites.forEach(sprite => sprite.hidden = true);
+      document.getElementById("ethanBackward").hidden = false;
+    } switch (event.code) {
+      case "ArrowDown":
+        imageSprites.forEach(sprite => sprite.hidden = true);
+        document.getElementById("ethanForward").hidden = false;
+    } switch (event.code) {
+      case "ArrowLeft":
+        imageSprites.forEach(sprite => sprite.hidden = true);
+        document.getElementById("ethanLeft").hidden = false;
+    } switch (event.code) {
+      case "ArrowRight":
+        imageSprites.forEach(sprite => sprite.hidden = true);
+        document.getElementById("ethanRight").hidden = false;
+      }
+    });
